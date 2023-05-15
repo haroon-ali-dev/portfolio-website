6b782ef3c8a1558d44a4999a9d9cbd6d3e3a218a
Haroon <haroon.ali.1104@gmail.com>
2023-05-15 20:53:57 +0100

    fix navbar overlapping content in mobile view

--- user/components/NavBar.js
+++ user/components/NavBar.js
@@ -6,11 +6,11 @@ import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     return (
-        <Navbar className={styles.navbar} bg="light" expand="lg">
+        <Navbar className={styles.myNavBar} bg="light" expand="lg">
             <Container>
                 <Navbar.Brand href="#home">H.ALI</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+                <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className={`${styles.linkContainer} me-auto ms-auto`}>
                         <Nav.Link href="#home">HOME</Nav.Link>
                         <Nav.Link href="#about-me">ABOUT ME</Nav.Link>
