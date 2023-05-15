f7a319eecfc170cc015baf3bd6141f36a217db90
Haroon <haroon.ali.1104@gmail.com>
2023-05-15 15:31:08 +0100

    create navbar

---
+++ user/components/NavBar.js
@@ -0,0 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function NavBar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Haroon Ali</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
--- user/pages/_app.js
+++ user/pages/_app.js
@@ -1,8 +1,12 @@
 import '@/styles/globals.css'
+import NavBar from '@/components/NavBar';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <header>
+        <NavBar />
+      </header>
       <Component {...pageProps} />
     </>
   );
