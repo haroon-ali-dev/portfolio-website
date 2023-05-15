5864d54358eef9fcb6322dfbf7939271f9fe500c
Haroon <haroon.ali.1104@gmail.com>
2023-05-15 17:41:01 +0100

    make hero section responsive

--- user/components/NavBar.js
+++ user/components/NavBar.js
@@ -8,9 +8,8 @@ export default function NavBar() {
     return (
         <Navbar className={styles.navbar} bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">HAROON ALI</Navbar.Brand>
+                <Navbar.Brand href="#home">H.ALI</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
                     <Nav className={`${styles.linkContainer} me-auto ms-auto`}>
                         <Nav.Link href="#home">HOME</Nav.Link>
@@ -21,7 +20,6 @@ export default function NavBar() {
                     </Nav>
                     <Nav.Link className={`${styles.contactBtn} ms-auto`} href="#contact">CONTACT ME</Nav.Link>
                 </Navbar.Collapse>
-                
             </Container>
         </Navbar>
     );
