c6c4a7a6b03f05b71bd7096c8248b10485b6abd2
Haroon <haroon.ali.1104@gmail.com>
2023-05-16 20:54:55 +0100

    create about me section

---
+++ user/pages/components/AboutMe.js
@@ -0,0 +1,14 @@
+import styles from './AboutMe.module.css';
+
+export default function AboutMe() {
+    return (
+        <section className={styles.section}>
+            <div className={styles.card}>
+                <h2>ABOUT ME</h2>
+                <p>Every business needs a top and creative talent; this is where I come into play.</p>
+                <p>Hi! This is Haroon Ali; I am a resourceful, highly-analytical, and talented individual, who has a solid understanding of web development and various programming languages along with frameworks i.e. JavaScript, HTML and Node.js.
+</p>
+            </div>
+        </section>
+    );
+}
--- user/pages/index.js
+++ user/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Hero from './components/Hero'
+import AboutMe from './components/AboutMe'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       </Head>
       <main>
         <Hero />
+        <AboutMe />
       </main>
     </>
   )
