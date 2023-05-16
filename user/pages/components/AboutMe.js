c8c9f1166a08ee74438cbc6f755c3dedabab0155
Haroon <haroon.ali.1104@gmail.com>
2023-05-16 21:04:47 +0100

    style about me section, make about me section responsive

--- user/pages/components/AboutMe.js
+++ user/pages/components/AboutMe.js
@@ -4,10 +4,11 @@ export default function AboutMe() {
     return (
         <section className={styles.section}>
             <div className={styles.card}>
-                <h2>ABOUT ME</h2>
+                <h2 className='mb-3'>ABOUT ME</h2>
                 <p>Every business needs a top and creative talent; this is where I come into play.</p>
-                <p>Hi! This is Haroon Ali; I am a resourceful, highly-analytical, and talented individual, who has a solid understanding of web development and various programming languages along with frameworks i.e. JavaScript, HTML and Node.js.
-</p>
+                <p>Hi! This is Haroon Ali; I am a resourceful, highly-analytical, and talented individual, who has a solid understanding of web development and various programming languages along with frameworks i.e. JavaScript, HTML and Node.js.</p>
+                <p>What makes me stand out are my solid skills in leading technology initiatives and streamlining business operations to facilitate major cost savings and productivity improvement. Moreover, I am also knowledgeable in writing reusable, testable, and efficient code.</p>
+                <p>I am also proficient in HTML, CSS, JavaScript, React.js, Node.js, PostgreSQL, Git & GitHub and Unit Testing. While others take pride in meeting all standards set before them, my passion is for delivering service that goes beyond organisational expectations, creating a win-win scenario for everyone involved.</p>
             </div>
         </section>
     );
