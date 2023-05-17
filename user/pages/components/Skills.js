e8ea36c21c5a03a3c5d5d14f12cc627a85c934fd
Haroon <haroon.ali.1104@gmail.com>
2023-05-17 21:10:01 +0100

    add content to skills section

--- user/pages/components/Skills.js
+++ user/pages/components/Skills.js
@@ -1,17 +1,42 @@
 import styles from './Skills.module.css';
+import { Code } from 'react-bootstrap-icons';
 
 export default function Skills() {
     return (
         <section className={styles.section}>
+            <h3 className='text-center mb-5'>SKILLS</h3>
             <ul className={styles.grid}>
                 <li>
-                    
+                    <Code className='mb-2' size={60} />
+                    <h5>HTML</h5>
                 </li>
                 <li>
-                    
+                    <Code className='mb-2' size={60} />
+                    <h5>CSS</h5>
                 </li>
                 <li>
-                    
+                    <Code className='mb-2' size={60} />
+                    <h5>JavaScript</h5>
+                </li>
+                <li>
+                    <Code className='mb-2' size={60} />
+                    <h5>React.js</h5>
+                </li>
+                <li>
+                    <Code className='mb-2' size={60} />
+                    <h5>Next.js</h5>
+                </li>
+                <li>
+                    <Code className='mb-2' size={60} />
+                    <h5>Node.js</h5>
+                </li>
+                <li>
+                    <Code className='mb-2' size={60} />
+                    <h5>PostgreSQL</h5>
+                </li>
+                <li>
+                    <Code className='mb-2' size={60} />
+                    <h5>Automated Testing</h5>
                 </li>
             </ul>
         </section>
