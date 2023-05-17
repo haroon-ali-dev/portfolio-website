8725ce84856f23bfa2d309b16505dda7334c870a
Haroon <haroon.ali.1104@gmail.com>
2023-05-17 13:25:56 +0100

    create skills grid

--- user/pages/components/Skills.js
+++ user/pages/components/Skills.js
@@ -2,8 +2,18 @@ import styles from './Skills.module.css';
 
 export default function Skills() {
     return (
-        <section>
-            
+        <section className={styles.section}>
+            <ul className={styles.grid}>
+                <li>
+                    
+                </li>
+                <li>
+                    
+                </li>
+                <li>
+                    
+                </li>
+            </ul>
         </section>
     );
 }
