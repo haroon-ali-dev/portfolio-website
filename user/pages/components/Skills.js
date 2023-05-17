d2084085e5b7d00f17653f313f89c22d97ced7ad
Haroon <haroon.ali.1104@gmail.com>
2023-05-17 21:50:46 +0100

    fix spacing

--- user/pages/components/Skills.js
+++ user/pages/components/Skills.js
@@ -7,35 +7,35 @@ export default function Skills() {
             <h3 className='text-center mb-5'>SKILLS</h3>
             <ul className={styles.grid}>
                 <li>
-                    <Code className='mb-2' size={60} />
+                    <Code className='mb-1' size={50} />
                     <h5>HTML</h5>
                 </li>
                 <li>
-                    <Code className='mb-2' size={60} />
+                    <Code className='mb-1' size={50} />
                     <h5>CSS</h5>
                 </li>
                 <li>
-                    <Code className='mb-2' size={60} />
+                    <Code className='mb-1' size={50} />
                     <h5>JavaScript</h5>
                 </li>
                 <li>
-                    <Code className='mb-2' size={60} />
+                    <Code className='mb-1' size={50} />
                     <h5>React.js</h5>
                 </li>
                 <li>
-                    <Code className='mb-2' size={60} />
+                    <Code className='mb-1' size={50} />
                     <h5>Next.js</h5>
                 </li>
                 <li>
-                    <Code className='mb-2' size={60} />
+                    <Code className='mb-1' size={50} />
                     <h5>Node.js</h5>
                 </li>
                 <li>
-                    <Code className='mb-2' size={60} />
+                    <Code className='mb-1' size={50} />
                     <h5>PostgreSQL</h5>
                 </li>
                 <li>
-                    <Code className='mb-2' size={60} />
+                    <Code className='mb-1' size={50} />
                     <h5>Automated Testing</h5>
                 </li>
             </ul>
