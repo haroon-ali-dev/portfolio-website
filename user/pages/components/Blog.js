3bb01fc42bba4818956e64813ac97ab4d3a1a046
Haroon <haroon.ali.1104@gmail.com>
2023-05-22 16:33:31 +0100

    complete blog section for home page

--- user/pages/components/Blog.js
+++ user/pages/components/Blog.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Image from "next/image";
 import Link from "next/link";
+import Moment from 'react-moment';
+import { CalendarFill } from 'react-bootstrap-icons';
 
 import styles from './Blog.module.css';
 
@@ -12,25 +14,27 @@ export default function Blog({ blogPostData }) {
             <h3 className='text-center mb-5'>BLOG</h3>
             <div className={styles.grid}>
                 {posts.map((item, i) => (
-                    <Link key={item.id} href={`/post/${item.id}`}>
-                        <article className={styles.article}>
-                            <div className={styles.imageContainer}>
-                                <Image
-                                    className={styles.image}
-                                    src={item.attributes.mainImage.data.attributes.url}
-                                    alt={item.attributes.title}
-                                    fill
-                                    priority={i < 3 ? true : false}
-                                    sizes="(max-width: 768px) 100vw,
+                    <article key={item.id} className={styles.article}>
+                        <div className={styles.imageContainer}>
+                            <Image
+                                className={styles.image}
+                                src={item.attributes.mainImage.data.attributes.url}
+                                alt={item.attributes.title}
+                                fill
+                                priority={i < 3 ? true : false}
+                                sizes="(max-width: 768px) 100vw,
                                     (max-width: 1200px) 50vw,
                                     33vw"
-                                />
-                            </div>
-                            <div className={styles.textContainer}>
-                                <p className={styles.itemHeading}>{item.attributes.title.toUpperCase()}</p>
-                            </div>
-                        </article>
-                    </Link>
+                            />
+                        </div>
+                        <div className={styles.textContainer}>
+                            <p className={styles.itemHeading}>{item.attributes.title.toUpperCase()}</p>
+                            <CalendarFill size={10} />
+                            <span className={styles.date}><Moment format='DD-MM-YYYY'>{item.attributes.date}</Moment></span>
+                            <p className={styles.body}>{item.attributes.body.substr(0, 100)}...</p>
+                            <Link className={styles.link} href={`/post/${item.id}`}>READ MORE</Link>
+                        </div>
+                    </article>
                 ))}
             </div>
         </section>
