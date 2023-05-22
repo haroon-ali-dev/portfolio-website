8d74459fdde704c6cf985f8fcafe0be055a6024c
Haroon <haroon.ali.1104@gmail.com>
2023-05-22 20:09:22 +0100

    add icons to footer

--- user/components/Footer.js
+++ user/components/Footer.js
@@ -1,9 +1,17 @@
+import Link from 'next/link';
+import { Github } from 'react-bootstrap-icons';
+import { Linkedin } from 'react-bootstrap-icons';
+
 import styles from "./Footer.module.css";
 
 export default function Footer() {
     return (
         <footer className={styles.section}>
             <p>GET IN TOUCH</p>
+            <ul className={styles.iconList}>
+                <li><Link href="https://github.com/haroon-ali-dev" target="_blank"><Github size={25} /></Link></li>
+                <li><Link href="https://www.linkedin.com/in/haroon-ali-dev" target="_blank"><Linkedin size={25} /></Link></li>
+            </ul>
         </footer>
     );
 }
