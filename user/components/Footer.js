63094309268d776052492a37ba8af3aa3f566a33
Haroon <haroon.ali.1104@gmail.com>
2023-05-23 19:57:42 +0100

    style footer top section

--- user/components/Footer.js
+++ user/components/Footer.js
@@ -7,11 +7,13 @@ import styles from "./Footer.module.css";
 export default function Footer() {
     return (
         <footer className={styles.section}>
-            <p>GET IN TOUCH</p>
+            <p style={{ fontWeight: "600" }}>GET IN TOUCH</p>
+            <Link className={styles.email} href="mailto:haroon.ali.1104@gmail.com">haroon.ali.1104@gmail.com</Link>
             <ul className={styles.iconList}>
                 <li><Link href="https://github.com/haroon-ali-dev" target="_blank"><Github size={25} /></Link></li>
                 <li><Link href="https://www.linkedin.com/in/haroon-ali-dev" target="_blank"><Linkedin size={25} /></Link></li>
             </ul>
+            <hr className={styles.divider} />
         </footer>
     );
 }
