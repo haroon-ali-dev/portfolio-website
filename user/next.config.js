3f0cbb18d511dd5088c304c6ea007fc558e6afed
Haroon <haroon.ali.1104@gmail.com>
2023-05-25 00:41:54 +0100

    change strapi host URL

--- user/next.config.js
+++ user/next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
     domains: ["res.cloudinary.com"],
   },
   env: {
-    strapiHost: (process.env.NODE_ENV === "development") ? "http://127.0.0.1:1337" : "https://portfolio-website-production-2c01.up.railway.app"
+    // strapiHost: (process.env.NODE_ENV === "development") ? "http://127.0.0.1:1337" : "https://portfolio-website-production-2c01.up.railway.app"
+    strapiHost: "https://portfolio-website-production-2c01.up.railway.app"
   }
 }
 
