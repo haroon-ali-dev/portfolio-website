c2464e5eb88f849f5d5840a83b941398ab275f4a
Haroon <haroon.ali.1104@gmail.com>
2023-05-25 22:00:01 +0100

    add animations to portfolio and blog section

--- user/pages/components/Blog.js
+++ user/pages/components/Blog.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Moment from 'react-moment';
 import { CalendarFill } from 'react-bootstrap-icons';
+import { motion } from "framer-motion"
 
 import styles from './Blog.module.css';
 
@@ -16,27 +17,34 @@ export default function Blog({ blogPostData }) {
                 <h3 className='text-center mb-5'>BLOG</h3>
                 <div className={styles.grid}>
                     {posts?.data.map((item, i) => (
-                        <article key={item.id} className={styles.article}>
-                            <div className={styles.imageContainer}>
-                                <Image
-                                    className={styles.image}
-                                    src={item.attributes.mainImage.data.attributes.url}
-                                    alt={item.attributes.title}
-                                    fill
-                                    priority={i < 3 ? true : false}
-                                    sizes="(max-width: 768px) 100vw,
-                                    (max-width: 1200px) 50vw,
-                                    33vw"
-                                />
-                            </div>
-                            <div className={styles.textContainer}>
-                                <p className={styles.itemHeading}>{item.attributes.title.toUpperCase()}</p>
-                                <CalendarFill size={10} />
-                                <span className={styles.date}><Moment format='DD-MM-YYYY'>{item.attributes.date}</Moment></span>
-                                <p className={styles.body}>{item.attributes.body.substr(0, 100)}...</p>
-                                <Link className={styles.link} href={`/blog/${item.id}`}>READ MORE</Link>
-                            </div>
-                        </article>
+                        <motion.div
+                            key={item.id}
+                            initial={{ opacity: 0, scale: 0.25 }}
+                            whileInView={{ opacity: 1, scale: 1 }}
+                            transition={{ ease: "easeOut", duration: 1 }}
+                        >
+                            <article className={styles.article}>
+                                <div className={styles.imageContainer}>
+                                    <Image
+                                        className={styles.image}
+                                        src={item.attributes.mainImage.data.attributes.url}
+                                        alt={item.attributes.title}
+                                        fill
+                                        priority={i < 3 ? true : false}
+                                        sizes="(max-width: 768px) 100vw,
+                                        (max-width: 1200px) 50vw,
+                                        33vw"
+                                    />
+                                </div>
+                                <div className={styles.textContainer}>
+                                    <p className={styles.itemHeading}>{item.attributes.title.toUpperCase()}</p>
+                                    <CalendarFill size={10} />
+                                    <span className={styles.date}><Moment format='DD-MM-YYYY'>{item.attributes.date}</Moment></span>
+                                    <p className={styles.body}>{item.attributes.body.substr(0, 100)}...</p>
+                                    <Link className={styles.link} href={`/blog/${item.id}`}>READ MORE</Link>
+                                </div>
+                            </article>
+                        </motion.div>
                     ))}
                 </div>
             </section>
--- user/pages/components/Portfolio.js
+++ user/pages/components/Portfolio.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion"
 
 import styles from './Portfolio.module.css';
 
@@ -15,24 +16,30 @@ export default function Portfolio({ portfolioData }) {
                 <div className={styles.grid}>
                     {portfolioItems?.data.map((item, i) => (
                         <Link key={item.id} href={`/portfolio/${item.id}`}>
-                            <article className={styles.article}>
-                                <div className={styles.imageContainer}>
-                                    <Image
-                                        className={styles.image}
-                                        src={item.attributes.image.data.attributes.url}
-                                        alt={item.attributes.title}
-                                        fill
-                                        priority={i < 3 ? true : false}
-                                        sizes="(max-width: 768px) 100vw,
-                                        (max-width: 1200px) 50vw,
-                                        33vw"
-                                    />
-                                </div>
-                                <div className={styles.textContainer}>
-                                    <p className={styles.itemHeading}>{item.attributes.title.toUpperCase()}</p>
-                                    <p className={styles.itemSubHeading}>{item.attributes.type}</p>
-                                </div>
-                            </article>
+                            <motion.div
+                                initial={{ opacity: 0, scale: 0.25 }}
+                                whileInView={{ opacity: 1, scale: 1 }}
+                                transition={{ ease: "easeOut", duration: 1 }}
+                            >
+                                <article className={styles.article}>
+                                    <div className={styles.imageContainer}>
+                                        <Image
+                                            className={styles.image}
+                                            src={item.attributes.image.data.attributes.url}
+                                            alt={item.attributes.title}
+                                            fill
+                                            priority={i < 3 ? true : false}
+                                            sizes="(max-width: 768px) 100vw,
+                                            (max-width: 1200px) 50vw,
+                                            33vw"
+                                        />
+                                    </div>
+                                    <div className={styles.textContainer}>
+                                        <p className={styles.itemHeading}>{item.attributes.title.toUpperCase()}</p>
+                                        <p className={styles.itemSubHeading}>{item.attributes.type}</p>
+                                    </div>
+                                </article>
+                            </motion.div>
                         </Link>
                     ))}
                 </div>
