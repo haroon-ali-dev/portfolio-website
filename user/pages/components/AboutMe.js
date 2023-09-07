9bd1397fc6d8aa33f73da5c4a25d2bef81486b74
Haroon <haroon.ali.1104@gmail.com>
2023-09-07 16:47:55 +0100

    change About Me text

--- user/pages/components/AboutMe.js
+++ user/pages/components/AboutMe.js
@@ -14,10 +14,7 @@ export default function AboutMe() {
                 >
                     <div className={styles.card}>
                         <h2 className={'text-center mb-4'}>ABOUT ME</h2>
-                        <p>Every business needs a top and creative talent; this is where I come into play.</p>
-                        <p>Hi! This is Haroon. I am a resourceful, highly-analytical, and talented individual, who has a solid understanding of web development and various programming languages along with frameworks i.e. JavaScript, HTML and Node.js.</p>
-                        <p>What makes me stand out are my solid skills in leading technology initiatives and streamlining business operations to facilitate major cost savings and productivity improvement. Moreover, I am also knowledgeable in writing reusable, testable, and efficient code.</p>
-                        <p>I am also proficient in HTML, CSS, JavaScript, React.js, Node.js, PostgreSQL, Git & GitHub and Unit Testing. While others take pride in meeting all standards set before them, my passion is for delivering service that goes beyond organisational expectations, creating a win-win scenario for everyone involved.</p>
+                        <p>Welcome to my portfolio! I'm a dedicated full-stack JavaScript developer with a strong passion for coding and innovation. My projects reflect my problem-solving skills, creativity, and attention to detail. I'm excited about my next tech career adventure and the opportunity to contribute my expertise to your team!</p>
                     </div>
                 </motion.div>
             </section>
